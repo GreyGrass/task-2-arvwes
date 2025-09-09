38b88adb6c87a5ba94bbb8ae67b573cd1b5240ad
lenarv <lenarv@ArvidsLenovo.localdomain>
2025-09-09 16:04:00 +0200

    made code more readable and logical, problem logic is not correct, should count stars on each rank not total numbers of stars

--- game_rank/src/main.rs
+++ game_rank/src/main.rs
@@ -1,57 +1,72 @@
 use std::io;
+
+const LOWEST_RANK: usize = 25;
+const LEGEND_RANK: usize = 0;
+const THREE_STAR_THRESHOLD: usize = 20;
+const FOUR_STAR_THRESHOLD: usize = 16;
+const FIVE_STAR_THRESHOLD: usize = 10;
 fn main() {
-    println!("Hello, world!");
-    const LOWEST_RANK: u8 = 25;
+  //  println!("Hello, world!");
+
     let mut winstreak = 0;
-    let mut stars = 0;
-    let mut temp: u8 = 0;
+    let mut stars: u8 = 0;
 
-    let mut total_stars_rank: Vec<u8> = Vec::new();
+    let mut stars_for_rank: Vec<u8> = Vec::new();
 
-    for i in (0..=25).rev() {
-        if  i == 25{
-            
-        } else if i >= 20 {
-            temp += 2
-        } else if i >= 16 {
-            temp += 3
-        } else if i >= 10 {
-            temp += 4
+    for rank in (LEGEND_RANK..=LOWEST_RANK).rev() {
+        let temp = if rank >= THREE_STAR_THRESHOLD {
+            2
+        } else if rank >= FOUR_STAR_THRESHOLD {
+            3
+        } else if rank >= FIVE_STAR_THRESHOLD {
+            4
+        } else if rank >= LEGEND_RANK {
+            5
         } else {
-            temp += 5
-        }
-        total_stars_rank.push(temp as u8);
+            0
+        };
+        stars_for_rank.push(temp);
     }
 
-    print!("{:?}", total_stars_rank);
+   // print!("{:?}", stars_for_rank);
 
     let mut input: String = String::new();
     io::stdin().read_line(&mut input).unwrap();
     let match_history: Vec<char> = input.trim().chars().collect::<Vec<char>>();
 
-    print!("{:?}", match_history);
+    //print!("{:?}", match_history);
+
     for game in &match_history {
         if game.eq_ignore_ascii_case(&'W') {
             stars += 1;
             winstreak += 1;
-            println!("{}", stars);
             if winstreak >= 3 {
                 stars += 1;
             }
         } else if game.eq_ignore_ascii_case(&'L') {
-            if stars > total_stars_rank[25 -20] {
-                 stars -= 1;
+            if get_rank(stars, &stars_for_rank) < 20 {
+                stars -= 1;
             }
-           
+
             winstreak = 0;
         }
     }
-      for i in (0..=25).rev() {
-        if stars >  total_stars_rank[i] {
-            
-           println!("you have {} stars, so you are rank{}",stars,  25- i);
-           println!("{}", total_stars_rank[25 -20]);
-           return; 
+
+   /*  println!(
+        "you have {} stars, so you are rank{}",
+        stars,
+        get_rank(stars, &stars_for_rank)
+    );*/
+    println!("{}", get_rank(stars, &stars_for_rank));
+}
+fn get_rank(stars: u8, stars_for_rank: &Vec<u8>) -> usize {
+    let mut stars_needed_for_rank = 0;
+
+    for (i, &stars_for_this_rank) in stars_for_rank.iter().enumerate() {
+        stars_needed_for_rank += stars_for_this_rank as u8;
+        if stars <= stars_needed_for_rank {
+            return LOWEST_RANK - i; //current rank
         }
     }
+    return 25;
 }
