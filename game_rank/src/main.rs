1324731bcc05fff92353dabb784d6e7cf06a8a9a
lenarv <lenarv@ArvidsLenovo.localdomain>
2025-09-10 15:38:43 +0200

    fixed loosing logic, now its overcomplicated.., and addedkattis image file (does pass all tests)

--- game_rank/src/main.rs
+++ game_rank/src/main.rs
@@ -9,19 +9,20 @@ fn main() {
     //  println!("Hello, world!");
 
     let mut winstreak = 0;
-    let mut curent_stars: u8 = 0;
-    let mut curent_rank: u8 = LOWEST_RANK.try_into().unwrap();
+    let mut losestreak = 0;
+    let mut curent_stars: u32 = 0;
+    let mut curent_rank: u32 = LOWEST_RANK.try_into().unwrap();
 
-    let mut stars_for_rank: Vec<u8> = Vec::new();
+    let mut stars_for_rank: Vec<u32> = Vec::new();
 
     for rank in (LEGEND_RANK..=LOWEST_RANK).rev() {
-        let temp = if rank >= THREE_STAR_THRESHOLD {
+        let temp = if rank > THREE_STAR_THRESHOLD {
             2
-        } else if rank >= FOUR_STAR_THRESHOLD {
+        } else if rank > FOUR_STAR_THRESHOLD {
             3
-        } else if rank >= FIVE_STAR_THRESHOLD {
+        } else if rank > FIVE_STAR_THRESHOLD {
             4
-        } else if rank >= LEGEND_RANK {
+        } else if rank > LEGEND_RANK {
             5
         } else {
             0
@@ -40,37 +41,41 @@ fn main() {
     for game in &match_history {
         if game.eq_ignore_ascii_case(&'W') {
             winstreak += 1;
+            losestreak =0;
 
             if winstreak >= 3 {
                 curent_stars += 2;
             } else {
                 curent_stars += 1;
+                
             }
-            if curent_rank > get_rank(curent_stars, &stars_for_rank) {
-                curent_rank = get_rank(curent_stars, &stars_for_rank);
-               // println!("rank updated to {}", curent_rank);
-            }
+            curent_rank = get_rank(curent_stars, &stars_for_rank);
+            
         } else if game.eq_ignore_ascii_case(&'L') {
-            if get_rank(curent_stars, &stars_for_rank) <= 20 {
+            losestreak +=1;
+            winstreak = 0;
+            if curent_stars != 0 && get_rank(curent_stars-1, &stars_for_rank) <= 20 && curent_rank as usize != LEGEND_RANK{
+               
                 curent_stars -= 1;
-                if curent_rank > get_rank(curent_stars, &stars_for_rank) {
+                if losestreak >=2 {
                     curent_rank = get_rank(curent_stars, &stars_for_rank);
                 }
+                
             }
 
-            winstreak = 0;
+            
         }
     }
 
-    println!("stars: {}", curent_stars);
+   // println!("stars: {}", curent_stars);
     println!("{}", curent_rank);
 }
-fn get_rank(curent_stars: u8, stars_for_rank: &Vec<u8>) -> u8 {
+fn get_rank(curent_stars: u32, stars_for_rank: &Vec<u32>) -> u32 {
     let mut stars_needed_for_rank = 0;
 
     for (i, &stars_for_this_rank) in stars_for_rank.iter().enumerate() {
-        stars_needed_for_rank += stars_for_this_rank as u8;
-        if curent_stars < stars_needed_for_rank  {
+        stars_needed_for_rank += stars_for_this_rank as u32;
+        if curent_stars <= stars_needed_for_rank  {
             return (LOWEST_RANK - i).try_into().unwrap(); //current rank
         }
     }
